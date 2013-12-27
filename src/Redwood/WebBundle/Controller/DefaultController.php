853579ef6fe048396cd0fb528e2b387c02517d25
zhouzy <zhouzy@bss360.com>
2013-12-27 16:36:08 +0800

    fixed 登录

--- src/Redwood/WebBundle/Controller/DefaultController.php
+++ src/Redwood/WebBundle/Controller/DefaultController.php
@@ -4,10 +4,12 @@ namespace Redwood\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class DefaultController extends BaseController
 {
     public function indexAction()
     {
+    	$user = $this->getCurrentUser();
+    	var_dump($user);
         return $this->render('RedwoodWebBundle:Default:index.html.twig', array('name' => 'testtest'));
     }
 }
